98df144ff8af5da3891ece7e795e9cc1879d6451
ashish-205 <85436925+ashish-205@users.noreply.github.com>
2021-10-02 22:08:19 +0530

    Add files via upload

---
+++ Practical3/Task1/noheader.cpp
@@ -0,0 +1,8 @@
+#define MAX 100
+using namespace std;
+
+int main()
+{
+  cout<<"Hello World"<<MAX;
+  return 0;
+}
---
+++ Practical3/Task2/doth .cpp
@@ -0,0 +1,9 @@
+#include<iostream.h>
+#define MAX 100
+using namespace std;
+
+int main()
+{
+ cout<<"Hello World "<<MAX;
+ return 0;
+}
